6778b5bc4c823baaa03ad5ab342311eeed3ee2b8
cjwelch <connor.welch@bestmile.com>
2018-07-25 15:25:58 -0400

    updated config

--- src/bestmile/config/config.js
+++
@@ -1,9 +0,0 @@
-export const baseURL = "https://api.partner.gce1.bestmile.io",
-  userID = "USER_ID",
-  iOS_userID = "iOS_USER_ID",
-  android_userID = "ANDROID_USER_ID",
-  siteID = "SITE_ID",
-  apiKey = "BESTMILE-API-KEY",
-  domainID = DOMAIN_ID,
-  mapsURL = "https://maps.googleapis.com/maps/api/geocode/json?",
-  mapsAPIKey = "GOOGLE-MAPS-API-KEY";
